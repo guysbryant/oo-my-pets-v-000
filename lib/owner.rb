415536d23067a3aa4e775dc8bd32643c50ac5b77
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-04 23:55:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,8 +53,8 @@ class Owner
   def sell_pets
     @pets.each {|type, pets| 
       pets.each {|pet| 
-        pet.mood = "nervous"} 
-      pets.clear}
+        pet.mood = "nervous"
+      pets.clear}}
   end
   
 end
