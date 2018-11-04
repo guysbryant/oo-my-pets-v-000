c37b6d1673396c13048a5e112cad853489e8b30f
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-04 22:58:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,15 +4,11 @@ class Owner
   @@all = []
   
   def initialize
-    self.class.all << self
-  end
-  
-  def self.all
-    @@all
+   
   end
   
   def self.reset_all
-    self.all.size = 0
+    
   end
   
 end
